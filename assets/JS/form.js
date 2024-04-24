5b20ad3b89ffd289f38f97860fd7bceb0101bd19
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-04-24 01:27:44 -0500

    set up the local storage, button has location fix to blog.html. added an alert to fill all the fields

--- assets/JS/form.js
+++ assets/JS/form.js
@@ -1,15 +1,30 @@
 const formElement = document.querySelector('form')
 
+ 
 
 // Elements from form
 const handleFormSubmit = function(event) {
     event.preventDefault();
   
 const userName = document.querySelector('#postUsername').value.trim();
-
 const titlePost = document.querySelector('#postTitle').value.trim();
-
 const contentPost = document.querySelector('#postContent').value.trim();
+// Data local storage
+if (userName && titlePost && contentPost) {
+    const blogPost = {
+        username: userName,
+        title: titlePost,
+        post: contentPost
+    };
+
+    let existingPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
+    existingPosts.push(blogPost);
+    localStorage.setItem('blogPosts', JSON.stringify(existingPosts));
+
+    window.location.href = "./blog.html"; 
+} else { 
+    alert('Please fill in all the fields.');
+}
 
 console.log(userName);
 console.log(titlePost);

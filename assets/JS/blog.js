58870d519a816542731adf0bed74e2f7a474a4a8
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-04-24 20:54:07 -0500

    I applied this style grid-template-columns: repeat(3, 1fr); specifies as a space-separated track. better look

--- assets/JS/blog.js
+++ assets/JS/blog.js
@@ -6,18 +6,6 @@ const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
 
 const blogPostsContainer = document.getElementById('blogPosts');
 
-//Display info blog post
-// blogPosts.forEach (post => {
-//     const postElement = document.createElement('main');
-//     postElement.innerHTML = `
-//     <h2>${post.title}</h2>
-//     <p><strong>By:</strong> ${post.username}</p>
-//     <p>${post.post}</p>
-//     <hr> 
-//     `;
-//     blogPostsContainer.appendChild(postElement);
-// });
-
 
 // Display info blog post - style card elements
 

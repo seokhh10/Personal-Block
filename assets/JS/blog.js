678b0977d288e8376d127933282163c01f7730a6
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-04-24 20:20:06 -0500

    buttom BACK is working. added and eventlistener and location.href. content boxes are styled

--- assets/JS/blog.js
+++ assets/JS/blog.js
@@ -7,19 +7,30 @@ const blogPosts = JSON.parse(localStorage.getItem('blogPosts')) || [];
 const blogPostsContainer = document.getElementById('blogPosts');
 
 //Display info blog post
-blogPosts.forEach (post => {
+// blogPosts.forEach (post => {
+//     const postElement = document.createElement('main');
+//     postElement.innerHTML = `
+//     <h2>${post.title}</h2>
+//     <p><strong>By:</strong> ${post.username}</p>
+//     <p>${post.post}</p>
+//     <hr> 
+//     `;
+//     blogPostsContainer.appendChild(postElement);
+// });
+
+
+// style card elements
+
+blogPosts.forEach(post => {
     const postElement = document.createElement('div');
+    postElement.classList.add('blog-card');
     postElement.innerHTML = `
-    <h2>${post.title}</h2>
-    <p><strong>By:</strong> ${post.username}</p>
-    <p>${post.post}</p>
-    <hr> 
-    `
+    <h2 class="blog-title">${post.title}</h2>
+    <p class="blog-user"><strong>By:</strong> ${post.username}</p>
+    <p class="blog-content">${post.post}</p>
+    <hr>`;
     blogPostsContainer.appendChild(postElement);
 });
-
-
-
 
 backButtom.addEventListener("click", () => {
     location.href = "./index.html";

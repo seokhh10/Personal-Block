191b92af0f47df6862a28f0c2d1b818ee3203ad5
seokhh10 <ernesto.hidalgo.hdz@gmail.com>
2024-04-25 23:53:33 -0500

    moving files around

--- logic.js
+++ logic.js
@@ -13,5 +13,5 @@ themeChangerButton.addEventListener("click", function() {
 
 
 postButton.addEventListener("click", () => {
-    window.location.href = "/blog.html";
+    window.location.href = "blog.html";
 })
